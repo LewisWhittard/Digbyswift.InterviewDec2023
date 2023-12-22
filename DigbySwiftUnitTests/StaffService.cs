4e1af0ddc54e9dbb80e0cb5a3cb5350f9d549464
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2023-12-22 12:25:44 +0000

    Staff Service

--- DigbySwiftUnitTests/StaffService.cs
+++ DigbySwiftUnitTests/StaffService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Digbyswift.InterviewDec2023.Infrastructure;
+using Digbyswift.InterviewDec2023.Infrastructure.BuissnessLogic.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +13,52 @@ namespace DigbySwiftUnitTests
         [Fact]
         public void AllStaff()
         {
+            StaffService sS = new StaffService();
 
+            var Data = sS.AllStaff();
+
+            Assert.Equal(123, Data[0].Id);
+            Assert.Equal("Kieron McIntyre", Data[0].FullName);
+            Assert.Equal("kieron@digbyswift.com", Data[0].Email);
+            Assert.Equal("Owner/Lead Developer", Data[0].JobTitle);
+            Assert.Equal("Code", Data[0].Likes[0]);
+            Assert.Equal("Karate", Data[0].Likes[1]);
+
+            Assert.Equal(556, Data[1].Id);
+            Assert.Equal("Joe Earnshaw", Data[1].FullName);
+            Assert.Equal("joe@digbyswift.com", Data[1].Email);
+            Assert.Equal("Senior Developer", Data[1].JobTitle);
+            Assert.Equal(null, Data[1].Likes);
+
+            Assert.Equal(838, Data[2].Id);
+            Assert.Equal("Owen Manby", Data[2].FullName);
+            Assert.Equal("owen@digbyswift.com", Data[2].Email);
+            Assert.Equal(null, Data[2].JobTitle);
+            Assert.Equal("Tintin", Data[2].Likes[0]);
+            Assert.Equal("Asterix", Data[2].Likes[1]);
         }
 
         [Fact]
         public void GetStaffById()
         {
+            StaffService sS = new StaffService();
 
+            var Data0 = sS.GetStaffById(123);
+            var Data1 = sS.GetStaffById(838);
+
+            Assert.Equal(123, Data0.Id);
+            Assert.Equal("Kieron McIntyre", Data0.FullName);
+            Assert.Equal("kieron@digbyswift.com", Data0.Email);
+            Assert.Equal("Owner/Lead Developer", Data0.JobTitle);
+            Assert.Equal("Code", Data0.Likes[0]);
+            Assert.Equal("Karate", Data0.Likes[1]);
+
+            Assert.Equal(838, Data1.Id);
+            Assert.Equal("Owen Manby", Data1.FullName);
+            Assert.Equal("owen@digbyswift.com", Data1.Email);
+            Assert.Equal(null, Data1.JobTitle);
+            Assert.Equal("Tintin", Data1.Likes[0]);
+            Assert.Equal("Asterix", Data1.Likes[1]);
         }
 
 
