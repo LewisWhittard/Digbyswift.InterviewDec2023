3aa9b26775c8a8c8c76a0640ef476ca3bb12d660
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2023-12-29 23:17:14 +0000

    Camel casing HomeIndexViewModelUnitTests

--- DigbySwiftUnitTests/HomeIndexViewModel.cs
+++ DigbySwiftUnitTests/HomeIndexViewModel.cs
@@ -7,10 +7,10 @@ namespace DigbySwiftUnitTests
     public class HomeIndexViewModelUnitTests
     {
         [Fact]
-        public void HomeIndexViewModelUnitTest()
+        public void ShouldCreateHomeIndexViewModelWithCorrectData()
         {
-            List<Staff> NoStaff = new List<Staff>();
-            List<Staff> staff = new List<Staff>();
+            List<Staff> noStaff = new List<Staff>();
+            List<Staff> listStaff = new List<Staff>();
 
             string[] likes1 = new string[] { "Swimming", "Running" };
             Staff staffRow1 = new Staff(0, "Donald Drake", "D.D@digbyswift.com", "Developer", likes1);
@@ -18,29 +18,29 @@ namespace DigbySwiftUnitTests
             Staff staffRow2 = new Staff(1, "Mary Beach", "M.B@digbyswift.com", "Senior Developer", likes2);
 
 
-            staff.Add(staffRow1);
-            staff.Add(staffRow2 );
+            listStaff.Add(staffRow1);
+            listStaff.Add(staffRow2 );
 
-            HomeIndexViewModel NoStaffViewModel = new HomeIndexViewModel(NoStaff);
-            HomeIndexViewModel StaffViewModel = new HomeIndexViewModel(staff);
+            HomeIndexViewModel noStaffViewModel = new HomeIndexViewModel(noStaff);
+            HomeIndexViewModel staffViewModel = new HomeIndexViewModel(listStaff);
 
-            Assert.Equal(-1, NoStaffViewModel.ListStaffViewModel[0].Id);
-            Assert.Equal("None", NoStaffViewModel.ListStaffViewModel[0].FullName);
-            Assert.Equal("None", NoStaffViewModel.ListStaffViewModel[0].Email);
-            Assert.Equal("None", NoStaffViewModel.ListStaffViewModel[0].JobTitle);
-            Assert.Equal("None", NoStaffViewModel.ListStaffViewModel[0].Likes);
+            Assert.Equal(-1, noStaffViewModel.ListStaffViewModel[0].Id);
+            Assert.Equal("None", noStaffViewModel.ListStaffViewModel[0].FullName);
+            Assert.Equal("None", noStaffViewModel.ListStaffViewModel[0].Email);
+            Assert.Equal("None", noStaffViewModel.ListStaffViewModel[0].JobTitle);
+            Assert.Equal("None", noStaffViewModel.ListStaffViewModel[0].Likes);
 
-            Assert.Equal(0, StaffViewModel.ListStaffViewModel[0].Id);
-            Assert.Equal("Donald Drake", StaffViewModel.ListStaffViewModel[0].FullName);
-            Assert.Equal("D.D@digbyswift.com", StaffViewModel.ListStaffViewModel[0].Email);
-            Assert.Equal("Developer", StaffViewModel.ListStaffViewModel[0].JobTitle);
-            Assert.Equal("Swimming, Running", StaffViewModel.ListStaffViewModel[0].Likes);
+            Assert.Equal(0, staffViewModel.ListStaffViewModel[0].Id);
+            Assert.Equal("Donald Drake", staffViewModel.ListStaffViewModel[0].FullName);
+            Assert.Equal("D.D@digbyswift.com", staffViewModel.ListStaffViewModel[0].Email);
+            Assert.Equal("Developer", staffViewModel.ListStaffViewModel[0].JobTitle);
+            Assert.Equal("Swimming, Running", staffViewModel.ListStaffViewModel[0].Likes);
 
-            Assert.Equal(1, StaffViewModel.ListStaffViewModel[1].Id);
-            Assert.Equal("Mary Beach", StaffViewModel.ListStaffViewModel[1].FullName);
-            Assert.Equal("M.B@digbyswift.com", StaffViewModel.ListStaffViewModel[1].Email);
-            Assert.Equal("Senior Developer", StaffViewModel.ListStaffViewModel[1].JobTitle);
-            Assert.Equal("Walking, Camping", StaffViewModel.ListStaffViewModel[1].Likes);
+            Assert.Equal(1, staffViewModel.ListStaffViewModel[1].Id);
+            Assert.Equal("Mary Beach", staffViewModel.ListStaffViewModel[1].FullName);
+            Assert.Equal("M.B@digbyswift.com", staffViewModel.ListStaffViewModel[1].Email);
+            Assert.Equal("Senior Developer", staffViewModel.ListStaffViewModel[1].JobTitle);
+            Assert.Equal("Walking, Camping", staffViewModel.ListStaffViewModel[1].Likes);
         }
     }
 }
